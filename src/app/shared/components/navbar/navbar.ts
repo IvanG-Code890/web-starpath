5ceb72a2a92504ba0adf089cdd9037817ab2f8f6
Ataik7 <wpablogallegodelgado@gmail.com>
2025-11-21 18:14:38 +0100

    Problema en las rutas, botones de inicio/registrarse quitados al iniciar el user

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { supabase } from '../../../core/services/supabase.config';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  imports: [RouterLink, RouterLinkActive],
+  styleUrls: ['./navbar.css']
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  user: any = null;
 
+  async ngOnInit() {
+    const { data: { user } } = await supabase.auth.getUser();
+    this.user = user;
+  }
+
+  async logout() {
+    await supabase.auth.signOut();
+    window.location.href = '/'; // o redirige a login
+  }
 }
