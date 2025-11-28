ff4da2ecf355980fe7a0ba96235ca08ea8e66407
Iván <lvt89045@gmail.com>
2025-11-28 09:05:55 +0100

    Pruebas unitarias de Login y Register completadas todas las pruebas unitarias básicas de los componentes funcionan

--- src/app/features/pages/login/login.spec.ts
+++ src/app/features/pages/login/login.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Login } from './login';
+import { provideRouter } from '@angular/router';
+import { supabase } from '../../../core/services/supabase.config';
+import { FormsModule } from '@angular/forms';
 
 describe('Login', () => {
   let component: Login;
   let fixture: ComponentFixture<Login>;
 
+  beforeEach(async () =>{
+    spyOn(supabase.auth, "signInWithPassword").and.returnValue(
+      Promise.resolve({
+        data: { user: {} as any, session: {} as any }, 
+        error: null
+      })
+    )
+  });
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Login]
+      imports: [Login, FormsModule],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
@@ -20,4 +31,6 @@ describe('Login', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
 });
--- src/app/features/pages/register/register.spec.ts
+++ src/app/features/pages/register/register.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Register } from './register';
+import { provideRouter } from '@angular/router';
+import { supabase } from '../../../core/services/supabase.config';
+import { FormsModule } from '@angular/forms';
 
 describe('Register', () => {
   let component: Register;
   let fixture: ComponentFixture<Register>;
 
+  beforeEach(async () =>{
+    spyOn(supabase.auth, "signUp").and.returnValue(
+      Promise.resolve({
+        data: {user : {} as any, session: null},
+        error: null
+      })
+    )
+  })
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Register]
+      imports: [Register, FormsModule],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
